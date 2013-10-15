e44fb958f47d4f72daf8debab9606ed8da5aed87
Mike Schwörer <mailport@mikescher.de>
2013-10-15 12:46:35 +0200

    First Website Test (With 1 Servlet)

---
+++ src/studyTrade/servlets/MainPage.java
@@ -0,0 +1,49 @@
+package studyTrade.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MainPage
+ */
+@WebServlet("/MainPage")
+public class MainPage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MainPage() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	      response.setContentType( "text/html" );
+	      PrintWriter out = response.getWriter();
+	      out.println( "<html>" );
+	      out.println( "<h3> Hallo, mein erstes Servlet meldet sich </h3>" );
+	      out.println( "<a href='http://www.google.de/'>zur&uuml;ck</a>" );
+	      out.println( "</html>" );
+	      out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
